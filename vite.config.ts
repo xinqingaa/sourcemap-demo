a0cf42dab6764fb678871771d6c09506db7da697
lrq <1162574925@qq.com>
2025-06-24 17:35:47 +0800

    feat:  修改打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,22 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  base: './',
+  plugins: [vue(), vueJsx()],
   build: {
+    outDir: 'dist',
     sourcemap: true,
-    outDir: "dist",
-    emptyOutDir: true,
-    rollupOptions: {}
+    emptyOutDir: true
+    // rollupOptions: {
+    //   input: ['src/index.html']
+    // }
   },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
