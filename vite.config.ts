ec5c35dc88d097637b8853ac6742d9cfbf67b12b
lrq <1162574925@qq.com>
2025-06-24 17:51:32 +0800

    feat:  修改打包配置_3

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/sourcemap-demo/",
   plugins: [
     vue(),
     vueJsx(),
